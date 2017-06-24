f60ceb31f9c768b79fb6e5920c07dd9533efe995
Pablo Victor <pablo.santos@ccc.ufcg.edu.br>
2017-06-24 14:24:45 -0300

    Adicionada as validações dos campos e mensagens de erro

--- app/core/app.routes.js
+++ app/core/app.routes.js
@@ -2,10 +2,10 @@ app.config(function ($routeProvider) {
     $routeProvider.when("/",{
         templateUrl: "view/search.html",
         controller : "searchCtrl"
-    }).when("/queixa/cadastro", {
-        templateUrl: "view/cadastraQueixa.html",
-        controller : "cadastraQueixaCtrl",
-        controllerAs: "cadastraQueixaCtrl"
+    }).when("/complaint/register", {
+        templateUrl: "view/registerComplaint.html",
+        controller : "registerComplaintCtrl",
+        controllerAs: "registerComplaintCtrl"
     }).otherwise({
         redirectTo: '/'
     });
---
+++ app/core/controller/registerComplaintController.js
@@ -0,0 +1,14 @@
+app.controller("registerComplaintCtrl", function (ComplaintService, $window) {
+    var self = this;
+
+    self.registerComplaint = function (complaint) {
+        ComplaintService.openComplaint(complaint)
+            .then(function success(response, status, headers, config){
+                $window.alert("Queixa adicionada com sucesso! " 
+                + "Você pode acompanhar a queixa com esse código: " + response.data.id);
+            }, function error(data, status, headers, config){
+                console.log(data);
+                $window.alert("Problemas ao tentar adicionar queixa.");
+            });            
+    }
+});
