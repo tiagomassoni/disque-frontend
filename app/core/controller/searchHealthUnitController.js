dd55c7a15d948ab743bb5e265e39178452432255
Pablo Santos <pablo.santos@ccc.ufcg.edu.br>
2017-07-03 08:24:43 -0300

    Tratando erro quando uma unidade de saude não é encontrada

--- app/core/controller/searchHealthUnitController.js
+++ app/core/controller/searchHealthUnitController.js
@@ -9,9 +9,12 @@ app.controller("searchHealthUnitCtrl", function (HealthUnitService) {
             .then(function success(response) {
                 self.units = [];
                 self.units.push(response.data);
-            }, function error(error) {
-                console.log("Erro");
-                console.log(error);
+            }, function failed(error) {
+                if(error.data.status == 500) {
+                    console.log("Nenhuma unidade foi encontrada");
+                } else {
+                    console.log("Erro ao tentar se conectar com o servidor");
+                }
             });
     }
 });
