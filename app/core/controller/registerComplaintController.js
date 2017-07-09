4ab83bd84647ab190e50a4e3276c53fc86a44ab1
Pablo Victor <pablo.santos@ccc.ufcg.edu.br>
2017-07-09 14:04:39 -0300

    Remoção dos Services e das validações do frontend

--- app/core/controller/registerComplaintController.js
+++ app/core/controller/registerComplaintController.js
@@ -1,15 +1,11 @@
 app.controller("registerComplaintCtrl", function ($scope,$http) {
-
-    $scope.responseComplaintId = "";
-
     $scope.registerComplaint = function (complaint) {
         $http.post("http://localhost:5000/SpringBootRestApi/api/queixa/", JSON.stringify(complaint))
             .then(function success(response){
                 console.log("Queixa adicionada com sucesso!");
-                self.responseComplaintId = response.data.id;
             }, function error(error){
                 console.log(error);
-                toastr.error("Problemas ao tentar adicionar queixa.");
+                console.log("Problemas ao tentar adicionar queixa.");
             });            
     }
 });
--- app/core/values/appConfigValue.js
+++
@@ -1,3 +0,0 @@
-app.value("AppConfig",{
-    baseUrl: ""
-});
