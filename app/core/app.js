b084cd8f2bcb81909b355e183bba4e8023be668a
Pablo Victor <pablo.santos@ccc.ufcg.edu.br>
2017-07-09 13:48:56 -0300

    Remoção das validações do frontend

--- app/core/app.js
+++ app/core/app.js
@@ -1,1 +1,1 @@
-const app = angular.module("vs", ["ngRoute", "ngMessages","ngAnimate", "toastr"]);
+const app = angular.module("vs", ["ngRoute", "ngMessages","ngAnimate"]);
--- app/core/controller/registerComplaintController.js
+++ app/core/controller/registerComplaintController.js
@@ -1,12 +1,8 @@
 app.controller("registerComplaintCtrl", function ($scope,$http) {
-
-    $scope.responseComplaintId = "";
-
     $scope.registerComplaint = function (complaint) {
         $http.post("http://localhost:5000/SpringBootRestApi/api/queixa/", JSON.stringify(complaint))
             .then(function success(response){
                 console.log("Queixa adicionada com sucesso!");
-                self.responseComplaintId = response.data.id;
             }, function error(error){
                 console.log(error);
                 toastr.error("Problemas ao tentar adicionar queixa.");
