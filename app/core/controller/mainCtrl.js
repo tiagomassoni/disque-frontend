d91d367e06dddf52e6348dbd5200a20f1eb1212a
Pablo Victor <pablo.santos@ccc.ufcg.edu.br>
2017-07-14 19:21:48 -0300

    Modificação do tratamento da resposta do servidor na busca de unidade de saude

--- app/core/controller/mainCtrl.js
+++ app/core/controller/mainCtrl.js
@@ -47,12 +47,9 @@ app.controller("searchHealthUnitCtrl", function ($scope, $http) {
                 $scope.units.push(response.data);
                 console.log("Foram encontradas Unidades de saúde");
                 console.log(response.data);
-            }, function failed(error) {
-                if(error.data.status == 500) {
-                    console.log("Nenhuma unidade foi encontrada", "Erro");
-                } else {
-                    console.log("Erro ao tentar se conectar com o servidor", "Erro");
-                }
+            }, function failed(error) {    
+                console.log("Erro na busca de unidades");
+                console.log(error.data.errorMessage);
             });
     }
 });
